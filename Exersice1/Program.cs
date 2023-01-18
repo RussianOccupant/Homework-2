cc8af0e1a9941fc69746b6cc41168d148a89917d
AlexBystrov <sshbystrov@gmail.com>
2023-01-18 14:19:38 +0400

    Finish work ex1

--- Exersice1/Program.cs
+++ Exersice1/Program.cs
@@ -1,2 +1,8 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Напишите программу, которая принимает на вход трехзначное число и на выходе показывает вторую цифру данного числа
+Console.Write("Введите трехзначное число: ");
+int num = int.Parse(Console.ReadLine());
+int a = num / 100;
+int b = num % 10;
+int c = (num - a * 100 - b) / 10;
+
+Console.WriteLine($"{num} -> {c}");
