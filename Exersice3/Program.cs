e03f48e0d2f175e661d347c39a55139364dc7eb3
AlexBystrov <sshbystrov@gmail.com>
2023-01-18 14:42:33 +0400

    Added answer for exersice3

---
+++ Exersice3/Program.cs
@@ -0,0 +1,20 @@
+﻿// Напишите программу, которая принмает на вход цифру
+// обозначающую день недели, и проверяет, является ли этот день выходным
+Console.Clear();
+Console.Write("Введите порядковый номер дня недели: ");
+int num = int.Parse(Console.ReadLine());
+if (num > 7 || num <= 0)
+{
+    Console.WriteLine($"Введенная вами цифра ({num}) не соответствует порядковому номеру дня недели");
+}
+else
+{
+    if (num == 7 || num == 6)
+    {
+        Console.WriteLine($"{num} -> YES");
+    }
+    else 
+    {
+        Console.WriteLine($"{num} -> NO");
+    }
+}
